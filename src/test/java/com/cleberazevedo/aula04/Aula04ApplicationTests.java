432f2d934a2089059e684c2b96958007f5808d99
Cleber Evangelista Azevedo <cleberazevedo_@outlook.com>
2021-04-15 19:39:49 -0300

    Initial commit

---
+++ src/test/java/com/cleberazevedo/aula04/Aula04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleberazevedo.aula04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
